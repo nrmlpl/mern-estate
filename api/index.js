eec6c87b2db1ffff67d188a5ee5ad3c8f95c6754
Nirmal Pal <142670318+nrmlpl@users.noreply.github.com>
2024-01-03 07:12:50 +0530

    vvc

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('Connected to mongoose');
+}).catch ((err) => {
+    console.log(err);
+})
 
 const app = express();
 
